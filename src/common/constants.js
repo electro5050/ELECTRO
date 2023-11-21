a78f34c6587ece4a01f32d7f8d77fa99ddc4049e
S2T Candidate <muhammedalthaf.althaf@gmail.com>
2023-11-21 19:26:47 +0530

    fix: changed url

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,10 +1,10 @@
 
 
 const config = {
-    apiUrl: 'http://192.168.29.125:3001',
-    gameApiUrl: 'http://192.168.29.125:3000',
-    chatSocketUrl: 'http://192.168.29.125:3002',
-    gameSocketUrl: 'ws://192.168.29.125:5000',
+    apiUrl: 'http://65.1.2.246:3001',
+    gameApiUrl: 'http://13.234.120.127:3000',
+    chatSocketUrl: 'http://3.110.29.192:3002',
+    gameSocketUrl: 'ws://13.233.134.21:5000',
   };
   
   export default config;
