859820416407398886f6ed1fb643cf9ea5078044
muhammedalthaf <muhammedalthaf.althaf@gmail.com>
2023-12-10 14:32:40 +0530

    url update

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,10 +1,10 @@
 
 
 const config = {
-    apiUrl: 'http://localhost:3001',
-    gameApiUrl: 'http://192.168.29.85:3000',
-    chatSocketUrl: 'http://localhost:3002',
-    gameSocketUrl: 'ws://192.168.29.85:5000',
-  };
-  
-  export default config;
+  apiUrl: 'https://api.electro5050.com:3001',
+  gameApiUrl: 'https://gameapi.electro5050.com:3000',
+  chatSocketUrl: 'https://chatsocket.electro5050.com:3002',
+  gameSocketUrl: 'ws://gamesocket.electro5050.com:5000',
+};
+
+export default config;
