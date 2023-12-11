eb975b6c328277c521f74b02e0391636e90b1bdc
jubin <jubin3000@gmail.com>
2023-12-11 09:55:46 +0530

    socket wss

--- src/common/constants.js
+++ src/common/constants.js
@@ -4,7 +4,7 @@ const config = {
   apiUrl: 'https://api.electro5050.com',
   gameApiUrl: 'https://gameapi.electro5050.com',
   chatSocketUrl: 'https://chatsocket.electro5050.com',
-  gameSocketUrl: 'ws://gamesocket.electro5050.com:5000',
+  gameSocketUrl: 'wss://gamesocket.electro5050.com:5000',
 };
 
 export default config;
